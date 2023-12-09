9b469c08e13676ce87334a0918f0173895d76586
fluex4 <fluex04@gmail.com>
2023-12-09 23:36:09 +0530

    added type functionality and changed styles

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const img = document.getElementById("pfp");
 const name = document.getElementById("name");
 const type = document.getElementById("type");
 const imgType = document.getElementById("imgtype");
-const list = document.getElementById("list");
+const list = document.getElementById("mainlist");
 let imgs;
+
+let z =document.getElementById("select");
+fetch("https://pokeapi.co/api/v2/type/?count=20")
+.then(response=>{
+    return response.json();
+ })
+    .then(data=>{
+        console.log(data);
+
+        data.results.forEach(element => {
+            let opt = document.createElement("option");
+            opt.value=element.name;
+            opt.innerText=element.name;
+            z.appendChild(opt);
+        });
+})
+
 get=()=>{
     let data;
     let name=document.getElementById("inpt").value;
@@ -106,20 +123,60 @@ document.getElementById("inpt").addEventListener("keyup", function(event) {
 
 
 displayList=()=>{
-    fetch(" ")
+    let value = document.getElementById("select").value;
+    er.classList.add("hide");
+    card.classList.add("hide");
+    list.classList.remove("hide");
+    list.innerHTML="";
+    fetch(`https://pokeapi.co/api/v2/type/${value}`)
     .then(response=>{
         return response.json();
      })
         .then(data=>{
-            er.classList.remove("hide");
-            card.classList.add("hide");
-            
-            
-            
+            data.pokemon.forEach(p => {
+                let index= p.pokemon.url.indexOf("/pokemon/");
+                let id= p.pokemon.url.substr(index).slice(0, -1);
 
-         }).catch(err=>{
+                let d = document.createElement("div");
+                let img= document.createElement("img");
+                let pa=document.createElement("p");
+
+                d.classList.add("list");
+
+                d.onclick=function(){
+                    display(p.pokemon.name);
+                    let x = document.getElementById("btn2");
+                    x.click();
+                }
+                
+                pa.innerText=p.pokemon.name;
+                
+                
+               img.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/${id}.png`
+               
+               img.onerror=function(){
+                img.src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png";
+               }
+                
+                d.appendChild(img);
+                d.appendChild(pa);
+
+                document.getElementById("mainlist").appendChild(d);
+            });
+}).catch(err=>{
             console.log(err);
             if(err.status==404)
             er.innerText=`Status:${err.status}  No Such Pokemon found `
          });
+         function display(str){
+            document.getElementById("inpt").value=str;
+        }
+}
+checkerr=(id)=>{
+    let x = id.slice(9);
+    if(errlistt.includes(x)){
+        console.log(x);
+        return true;
+    }
+
 }
